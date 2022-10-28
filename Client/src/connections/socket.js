3b967bc8287f14c3f0e3cfedece21c2e8a2389c0
BenDaBeast22 <53120446+BenDaBeast22@users.noreply.github.com>
2022-10-28 19:40:17 -0400

    fixed socket connection

--- Client/src/connections/socket.js
+++ Client/src/connections/socket.js
@@ -1,8 +1,7 @@
 import { io } from 'socket.io-client';
 let socket;
-// if (process.env.NODE_ENV === "production") socket = io();
-// else socket = io("http://localhost:8000");
-socket = io("http://localhost:8000");
+if (process.env.NODE_ENV === "production") socket = io();
+else socket = io("http://localhost:8000");
 
 socket.on("CreateNewGame", statusUpdate => {
   console.log(`Created new game! Game Id: ${statusUpdate.gameId} Socket Id: ${statusUpdate.socketId}`);
